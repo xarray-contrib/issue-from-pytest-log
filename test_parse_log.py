582310fc7ad4693afe63840c759c881b53ed5f83
Justus Magin <keewis@posteo.de>
2022-11-07 13:25:22 +0100

    add a hypothesis-based test

---
+++ test_parse_log.py
@@ -0,0 +1,39 @@
+import re
+
+import hypothesis.strategies as st
+from hypothesis import given, note
+
+from parse_logs import PreformattedReport, parse_nodeid
+
+directory_re = r"(\w|-)+"
+path_re = re.compile(rf"/?({directory_re}(/{directory_re})*/)?test_[A-Za-z0-9_]+\.py")
+filepaths = st.from_regex(path_re, fullmatch=True)
+
+group_re = r"Test[A-Za-z0-9_]+"
+name_re = re.compile(rf"({group_re}::)*test_[A-Za-z0-9_]+")
+names = st.from_regex(name_re, fullmatch=True)
+
+variants = st.from_regex(re.compile(r"(\w+-)*\w+"), fullmatch=True)
+
+messages = st.text()
+
+
+def preformatted_reports():
+    return st.tuples(filepaths, names, variants | st.none(), messages).starmap(
+        PreformattedReport
+    )
+
+
+@given(filepaths, names, variants)
+def test_parse_nodeid(path, name, variant):
+    if variant is not None:
+        nodeid = f"{path}::{name}[{variant}]"
+    else:
+        nodeid = f"{path}::{name}"
+
+    note(f"nodeid: {nodeid}")
+
+    expected = {"filepath": path, "name": name, "variant": variant}
+    actual = parse_nodeid(nodeid)
+
+    assert actual == expected
